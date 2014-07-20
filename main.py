cdcb4d163cea58e70233f2aba24e68d5fc995f8f
kr4ng <steven.m.simoni@gmail.com>
2014-07-19 18:38:23 -0700

    d

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import os
-from flask import Flask
+import urllib3
+from flask import Flask, request, json
+
+from flask.ext import restful
+from flask.ext.jsonpify import jsonify
 
 app = Flask(__name__)
+api = restful.Api(app)
 
-@app.route('/')
-def main():
-    return 'Hello World!'
+class startjob(restful.Resource):
+    def post(self, ID):
+        json_data = request.get_json(force=True)
+        return None
+
+api.add_resource(merge, '/startjob')
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+    #for debugging
+    print 'lol'
